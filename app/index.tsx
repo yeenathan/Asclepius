6a86e2058a602e1da8a6a29742ce45fe764d8a50
Deluka <deluka.lgh@gmail.com>
2024-10-15 18:14:39 -0700

    kill me

--- app/index.tsx
+++ app/index.tsx
@@ -28,14 +28,13 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-// import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { EvaIconsPack } from '@ui-kitten/eva-icons'
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './navigation.component';
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack}/>
-    <ApplicationProvider {...eva} theme={eva.dark}>
+    <ApplicationProvider {...eva} theme={eva.light}>
       <AppNavigator/>
     </ApplicationProvider>
   </>
