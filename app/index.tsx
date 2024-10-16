030b97e5afd992172547ed4c313ef29b3c8d6a1f
Deluka <deluka.lgh@gmail.com>
2024-10-15 17:13:02 -0700

    adding buttons, colors and font to darkmode, setting up navigation with different pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, Button, IconRegistry, Icon } from '@ui-kitten/components';
+import { default as theme } from '../theme.json'
+import { default as mapping } from '../mapping.json';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.dark}>
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  <>
+  <IconRegistry icons={EvaIconsPack} />
+  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }} 
+    customMapping={mapping}>
+
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10}}>
       <Text>Welcome to UI Kitten</Text>
+      <Button size='tiny'>Help</Button>
+      <Button size='small'>HELP</Button>
+      <Button size='medium'>HELP</Button>
+      <Button size='large'>HELP</Button>
+      <Button size='giant'>HELP</Button>
     </Layout>
   </ApplicationProvider>
+  </>
 );
