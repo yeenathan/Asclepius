15c5838fa7508616b7aaa46bf6af7dfe2a0c3e03
yeenathan <yeenathan21@gmail.com>
2024-10-16 14:48:08 -0700

    implement temp theming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,12 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './navigation.component';
+import { default as theme } from './custom-theme.json';
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack}/>
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
       <AppNavigator/>
     </ApplicationProvider>
   </>
