3b4ad1471fb0315f26d641231b8de0174a3e9ff6
yeenathan <yeenathan21@gmail.com>
2024-10-16 16:17:51 -0700

    color theme and trying to add fonts (unsuccessful)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,17 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './navigation.component';
-import { default as colorTheme } from './custom-theme.json';
-
+import { default as colorTheme } from '../custom-theme.json';
+import { default as mapping } from "../mapping.json"
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack}/>
-    <ApplicationProvider {...eva} theme={{...eva.light, ...colorTheme}}>
+    <ApplicationProvider
+      {...eva}
+      theme={{...eva.light, ...colorTheme}}
+      // customMapping={mapping}
+    >
       <AppNavigator/>
     </ApplicationProvider>
   </>
