5c4c0628d957bce25cea3745eb97123606a8e6d7
Deluka <deluka.lgh@gmail.com>
2024-10-18 14:09:35 -0700

    fixing styling

--- app/details.component.js
+++ app/details.component.js
@@ -7,15 +7,9 @@ export const DetailsScreen = ({ navigation }) => {
   return (
     <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#f8f8f8", gap: 10}}>
       {/* <ScrollView className="scrollview"> */}
-        <Text category='h1'>DETAILS</Text>
-        <View style={{flexDirection: "row", alignItems: "center", flexWrap: "wrap", gap: 5}}>
-          <Button style={{  }} size='tiny'>Tiny</Button>
-          <Button style={{  }} size='small'>Small</Button>
-          <Button style={{  }} size='medium'>Medium</Button>
-          <Button style={{  }} size='large'>Large</Button>
-          <Button style={{  }} size='giant'>Giant</Button>
-        </View>
-        <View style={{ flexDirection: "column", alignItems: "center", gap: 8}}>
+        <Text category='h1'>Components</Text>
+        
+        <View style={{ flexDirection: "column", alignItems: "center", gap: 10}}>
           <Text category='h1'>H1 Header One</Text>
           <Text category='h2'>H2</Text>
           <Text category='h3'>H3</Text>
@@ -24,17 +18,17 @@ export const DetailsScreen = ({ navigation }) => {
           <Text category='h6'>H6</Text>
         </View>
 
-        <View style={{ flexDirection: "row"}}>
+        <View style={{ flexDirection: "row", gap: 5 }}>
           <Text category='S1'>S1</Text>
           <Text category='S2'>S2</Text>
         </View>
 
-        <View style={{ flexDirection: "row"}}>
+        <View style={{ flexDirection: "row", gap: 5 }}>
           <Text category='p1'>p1</Text>
           <Text category='p2'>p2</Text>
         </View>
 
-        <View style={{ flexDirection: "row"}}>
+        <View style={{ flexDirection: "row", gap: 5 }}>
           <Text category='c1'>C1</Text>
           <Text category='c2'>C2</Text>
         </View>
@@ -49,11 +43,20 @@ export const DetailsScreen = ({ navigation }) => {
           <Text appearance='alternative'>Alternative Text.....</Text>
         </View>
 
-        <View style={{}}>
+        <View style={{ }}>
           <Text status='primary'>Primary yipee</Text>
         </View>
+
       {/* </ScrollView> */}
     </Layout>
   );
 };
+
+{/* <View style={{flexDirection: "row", alignItems: "center", flexWrap: "wrap", gap: 5}}>
+          <Button style={{  }} size='tiny'>Tiny</Button>
+          <Button style={{  }} size='small'>Small</Button>
+          <Button style={{  }} size='medium'>Medium</Button>
+          <Button style={{  }} size='large'>Large</Button>
+          <Button style={{  }} size='giant'>Giant</Button>
+        </View> */}
 
