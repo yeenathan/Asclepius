6b7e10ddfbbb1ef02f11a17d05a4f818b9afde36
yeongtaek <144389087+yeonggtaek@users.noreply.github.com>
2024-10-17 19:24:22 -0700

    changing color

--- app/home.component.js
+++ app/home.component.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import { ApplicationProvider, Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import { default as theme } from './theme.json'; 
+import { default as mapping } from './mapping.json'; 
 
 export const HomeScreen = ({ navigation }) => {
 
@@ -9,10 +12,15 @@ export const HomeScreen = ({ navigation }) => {
   };
 
   return (
+    <ApplicationProvider 
+    {...eva}
+    theme={{ ...eva.dark, ...theme }}
+    customMapping={mapping}>
     <SafeAreaView style={{ flex: 1 }}>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', fontFamily: 'Poppins-Regular'}}>
         <Button onPress={navigateDetails}>OPEN DETAILS</Button>
       </Layout>
     </SafeAreaView>
+    </ApplicationProvider>
   );
 };
