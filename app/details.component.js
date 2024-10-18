e452aa5a1fb30bd48899ce0e11be48cf9fe9a744
Deluka <deluka.lgh@gmail.com>
2024-10-17 20:54:44 -0700

    adding text

--- app/details.component.js
+++ app/details.component.js
@@ -9,13 +9,30 @@ export const DetailsScreen = ({ navigation }) => {
         <Text category='h1'>DETAILS</Text>
       </Layout>
 
-      <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', gap: 5, marginLeft: 20, }}>
+      <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', gap: 5, marginLeft: 20, marginTop: 50}}>
         <Button style={{  }} size='tiny'>Tiny</Button>
         <Button style={{  }} size='small'>Small</Button>
         <Button style={{  }} size='medium'>Medium</Button>
         <Button style={{  }} size='large'>Large</Button>
         <Button style={{  }} size='giant'>Giant</Button>
       </Layout> 
+
+      <Layout style={{flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', marginLeft: 20, gap: 5, marginTop: -300 }}>
+        <Text category='h1'>H1</Text>
+        <Text category='h2'>H2</Text>
+        <Text category='h3'>H3</Text>
+        <Text category='h4'>H4</Text>
+        <Text category='h5'>H5</Text>
+        <Text category='h6'>H6</Text>
+        <Text category='S1'>S1</Text>
+        <Text category='S2'>S2</Text>
+        <Text category='p1'>p1</Text>
+        <Text category='p2'>p2</Text>
+        <Text category='c1'>C1</Text>
+        <Text category='c2'>C2</Text>
+        <Text category='label'>LABEL</Text>
+      </Layout>
+
     </SafeAreaView>
     
   );
