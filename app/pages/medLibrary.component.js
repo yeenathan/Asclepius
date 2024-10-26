aad6694c79d2ed05fe1e08e62e421c55b9919d08
Nathan <yeenathan21@gmail.com>
2024-10-26 00:56:03 -0700

    med library tab switching functionality

--- app/pages/medLibrary.component.js
+++ app/pages/medLibrary.component.js
@@ -1,18 +1,30 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View, Image } from 'react-native';
 import { Button, Layout, Text, Icon} from '@ui-kitten/components';
 import { Header } from "@/app/components/header"
 
 import { default as colorTheme } from "@/custom-theme.json"
 import { styles } from "@/app/stylesheet"
 
+import { LIBRARY_DATA } from "@/app/data/medData"
+
 export const MedFolder = ({navigation}) => { // med folder component
-  
+  const [data, setData] = useState(LIBRARY_DATA);
+
+  function handleSwitch(value) {
+    if (value) {
+      setData(LIBRARY_DATA);
+    }
+    else {
+      setData([LIBRARY_DATA[LIBRARY_DATA.length-1]]);
+    }
+  }
+
   const TabSwitch = () => {
     return (
       <View style={{flexDirection: "row", gap: 6}}>
-        <Button style={{flex: 1}}>Current</Button>
-        <Button style={{flex: 1}}>Archive</Button>
+        <Button onPress={() => handleSwitch(1)} style={{flex: 1}}>Current</Button>
+        <Button onPress={() => handleSwitch(0)} style={{flex: 1}}>Archive</Button>
       </View>
     )
   }
@@ -20,6 +32,25 @@ export const MedFolder = ({navigation}) => { // med folder component
   return (
     <Layout style={{flex: "1", backgroundColor: colorTheme['silver-white']}}>
       <TabSwitch />
+      <View style={{paddingHorizontal: "2.5rem", marginTop: "2rem", gap: "1rem"}}>
+        {
+          data.map((med, index) => {
+            return (
+              <Button
+                onPress={() => navigation.navigate("Info", {medication: med})}
+                style={{backgroundColor: colorTheme['light-blue'], ...styles.invisBorder}}
+                key={index}
+                children={() => (
+                  <View style={{flexDirection: "row", alignItems: "center"}}>
+                    <Image source={med.icon}/>
+                    <Text category='h2'>{med.name}</Text>
+                  </View>
+                )}
+              />
+            )
+          })
+        }
+      </View>
     </Layout>
   )
 }
@@ -46,14 +77,16 @@ export const EditInfoScreen = ({navigation}) => { // edit info screen
   )
 }
 
-export const InfoScreen = ({navigation}) => { // info screen
+export const InfoScreen = ({navigation, route}) => { // info screen
+  const medication = route.params.medication;
+
   return (
     <SafeAreaView style={{ flex: 1}}>
       <Header navigation={navigation}/>
       <Layout style={styles.masterLayout}>
-        <Text>Info</Text>
-        <Button onPress={() => navigation.navigate("Med Stack", {screen: "Edit Reminder"})}>Reminder</Button>
-        <Button onPress={() => navigation.navigate("Med Stack", {screen: "Edit Info"})}>Edit</Button>
+        <Text>{medication.name} Info</Text>
+        <Button onPress={() => navigation.navigate("Med Stack", {screen: "Edit Reminder", medication: medication})}>Reminder</Button>
+        <Button onPress={() => navigation.navigate("Med Stack", {screen: "Edit Info", medication: medication})}>Edit</Button>
       </Layout>
     </SafeAreaView>
   )
@@ -68,7 +101,7 @@ export const MedLibraryScreen = ({navigation}) => { // main med library screen
           <Icon style={{width: "40px"}} name="settings-2-outline"></Icon>
         </View>
       </Layout>
-      <MedFolder navigation={navigation} />
+      <MedFolder navigation={navigation}/>
     </SafeAreaView>
   );
 };
