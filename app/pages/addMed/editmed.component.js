4f5921900ce07019999961c47ad30502d780115e
Deluka <deluka.lgh@gmail.com>
2024-10-26 15:50:01 -0700

    adding edit medication page

--- app/pages/addMed/editmed.component.js
+++ app/pages/addMed/editmed.component.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { SafeAreaView } from "react-native";
-import { Text, Layout, Button } from "@ui-kitten/components";
+import { SafeAreaView, View} from "react-native";
+import { Text, Layout, Button, } from "@ui-kitten/components";
 import { Header } from '@/app/components/header';
 import { SuggestionSearch } from '@/app/components/suggestionSearch';
-
 import { default as colorTheme } from "@/custom-theme.json"
+import { MyButton } from '@/app/components/MyButton';
+import { styles } from '@/app/stylesheet';
+import { CurrentRenderContext } from '@react-navigation/native';
 
 export const EditScreen = ({navigation}) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Header navigation={navigation}/>
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: "2.5rem", backgroundColor: colorTheme['silver-white']}}>
-        <Text category='h1'>Medication</Text>
-        <SuggestionSearch />
-        <Button size='giant'>Confirm</Button>
+        <View style={{ justifyContent: 'center', alignItems: 'center', flexDirection: 'column', flex: 2}}>
+          <Text category='h6'>What is the medication name?</Text>
+          <Text category='c1'>Search or type your medication name</Text>
+        </View>
+
+        <View style={{ flex: 8, width: '70%'}}>
+          <SuggestionSearch />
+        </View>
+
+        <View style={{ flex: 3, width: '100%'}}>
+          <MyButton text="Confirm" styles={{...styles.orangerButton, ...styles.baseBigButton}} press={() => navigation.navigate('')} />
+        </View>
       </Layout>
     </SafeAreaView>
   );
