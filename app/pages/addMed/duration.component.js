35e5288cf5ac17c8e56b6877e8bd177eef14afbb
Deluka <deluka.lgh@gmail.com>
2024-10-27 23:28:26 -0700

    fixing error on duration page

--- app/pages/addMed/duration.component.js
+++ app/pages/addMed/duration.component.js
@@ -4,6 +4,7 @@ import { Button, Input, Layout, Text} from '@ui-kitten/components';
 import { MyButton } from '@/app/components/MyButton';
 import { styles } from '@/app/stylesheet';
 import { DisplayDropdown } from '../../components/displayDropdown';
+import { DoseMonth } from '@/app/components/dateNextDose';
 
 
 import { default as colorTheme } from "@/custom-theme.json"
@@ -14,6 +15,10 @@ import { Header } from '@/app/components/header';
 
 export const SetDuration = ({navigation}) => {
   const [value, setValue] = React.useState('');
+  const [value2, setValue2] = React.useState('');
+  const [value3, setValue3] = React.useState('');
+  const [value4, setValue4] = React.useState('');
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Header navigation={navigation} />
@@ -28,8 +33,16 @@ export const SetDuration = ({navigation}) => {
             value={value}
             onChangeText={nextValue => setValue(nextValue)}
             style={{width: "5rem"}}
-          />
-          <DisplayDropdown data={["Hours", "Days", "Months", "Years"]}/>
+          />    
+          <DoseMonth />
+
+          <Input
+            placeholder='Year'
+            value={value2}
+            onChangeText={nextValue => setValue2(nextValue)}
+            style={{width: "5rem"}}
+          />   
+          {/* <DisplayDropdown data={["Hours", "Days", "Months", "Years"]}/> */}
         </View>
 
         <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
@@ -39,11 +52,18 @@ export const SetDuration = ({navigation}) => {
         <View style={{ flex: 4, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: "1rem" }}>
             <Input
                 placeholder='#'
-                value={value}
-                onChangeText={nextValue => setValue(nextValue)}
+                value={value3}
+                onChangeText={nextValue => setValue3(nextValue)}
                 style={{width: "5rem"}}
             />
-            <DisplayDropdown data={["Hours", "Days", "Months", "Years"]}/>
+            <DoseMonth />
+            <Input
+                placeholder='#'
+                value={value4}
+                onChangeText={nextValue => setValue4(nextValue)}
+                style={{width: "5rem"}}
+            />
+            {/* <DisplayDropdown data={["Hours", "Days", "Months", "Years"]}/> */}
         </View>
         
         <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center', width: '100%' }}>
