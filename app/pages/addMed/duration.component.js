4acf3c781ed34b6ef185c11f0abd2505d3e1d644
Deluka <deluka.lgh@gmail.com>
2024-10-27 23:09:22 -0700

    making edit icon to edit icon page

---
+++ app/pages/addMed/duration.component.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { SafeAreaView, View, ScrollView, } from 'react-native';
+import { Button, Input, Layout, Text} from '@ui-kitten/components';
+import { MyButton } from '@/app/components/MyButton';
+import { styles } from '@/app/stylesheet';
+import { DisplayDropdown } from '../../components/displayDropdown';
+
+
+import { default as colorTheme } from "@/custom-theme.json"
+
+import { Header } from '@/app/components/header';
+
+
+
+export const SetDuration = ({navigation}) => {
+  const [value, setValue] = React.useState('');
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Header navigation={navigation} />
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: "2.5rem", backgroundColor: colorTheme['silver-white'], gap: 10}}>
+        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+          <Text category='h6'>Set date Duration</Text>
+        </View>
+
+        <View style={{ flex: 7, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: "1rem"}}>
+          <Input
+            placeholder='#'
+            value={value}
+            onChangeText={nextValue => setValue(nextValue)}
+            style={{width: "5rem"}}
+          />
+          <DisplayDropdown data={["Hours", "Days", "Months", "Years"]}/>
+        </View>
+
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Text category='h6'>From</Text>
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: "1rem" }}>
+            <Input
+                placeholder='#'
+                value={value}
+                onChangeText={nextValue => setValue(nextValue)}
+                style={{width: "5rem"}}
+            />
+            <DisplayDropdown data={["Hours", "Days", "Months", "Years"]}/>
+        </View>
+        
+        <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+          <MyButton text="Confirm" styles={{...styles.orangerButton, ...styles.baseBigButton}} press={() => navigation.navigate('Confirm Med')} />
+        </View>
+
+      </Layout>
+    </SafeAreaView>
+  );
+};
