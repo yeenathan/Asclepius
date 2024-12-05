84428a20fb4f49071a047fac627e966cb51a5b00
kennyharman <144394021+kennyharman@users.noreply.github.com>
2024-12-05 15:31:29 -0800

    adding the graphic fading animation, but so many errors :(

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
+    plugins: ['react-native-reanimated/plugin'
     ],
   };
 };
