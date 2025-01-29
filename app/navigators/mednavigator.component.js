ba7e46bf13bd670aea81b6018c8382b9e4cddff1
yeenathan <yeenathan21@gmail.com>
2025-01-28 19:39:06 -0800

    clean up

--- app/navigators/mednavigator.component.js
+++ app/navigators/mednavigator.component.js
@@ -1,17 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { AddScreen } from "@/app/pages/addMed/addmed.component"
 import { ScanScreen } from "@/app/pages/addMed/scanscreen.component"
-import { ManualNameEdit, ManualIntervalEdit, ManualDoseEdit, ExtraOptions, IconPick, MedConfirm, StartDatePick} from "@/app/pages/addMed/manualscreen.component"
-import { ConfirmScan } from "@/app/pages/addMed/scanconfirm.component"
-import { EditScreen } from "@/app/pages/addMed/editmed.component"
-import { EditInfoScreen, EditReminderScreen, MedDescription } from "@/app/pages/medLibrary.component"
-import { SetTime } from "@/app/pages/addMed/addtimeinterval.component"
-import { DosePerTime } from "@/app/pages/addMed/dosepertime.component"
-import { NextDose } from "@/app/pages/addMed/nextdose.component"
-import { TimeDose } from "@/app/pages/addMed/timenextdose.component"
 import { Onboarding } from '../pages/home.component';
-import { SetDuration } from "@/app/pages/addMed/duration.component"
-import { ScheduleDose } from "@/app/pages/addMed/time.component"
 import { SettingsScreen } from "@/app/pages/settings"
 
 
@@ -27,28 +17,6 @@ export const MedNavigator = () => {
       <Stack.Screen name="Add Med" component={AddScreen} options={{headerShown: false}}/>
       <Stack.Screen name="Scan" component={ScanScreen} options={{headerShown: false}}/>
       <Stack.Screen name="Settings" component={SettingsScreen} options={{headerShown: false}}/>
-
-{/* 
-      <Stack.Screen name="Manual Name" component={ManualNameEdit} options={{headerShown: false}}/>
-      <Stack.Screen name="Manual Interval" component={ManualIntervalEdit} options={{headerShown: false}}/>
-      <Stack.Screen name="Manual Dose" component={ManualDoseEdit} options={{headerShown: false}}/>
-      <Stack.Screen name="Start Date" component={StartDatePick} options={{headerShown: false}}/>
-      <Stack.Screen name='Extra Options' component={ExtraOptions} options={{headerShown: false}}/>
-      <Stack.Screen name='Icon Pick' component={IconPick} options={{headerShown: false}}/>
-      <Stack.Screen name='Med Confirm' component={MedConfirm} options={{headerShown: false}}/>
-
-      <Stack.Screen name="Confirm Scan" component={ConfirmScan} options={{headerShown: false}}/>
-
-      <Stack.Screen name="Edit Name" component={EditScreen} initialParams={{fromManual: false}} options={{headerShown: false}}/>
-      <Stack.Screen name="Edit Time" component={SetTime} initialParams={{fromManual: false}} options={{headerShown: false}} />
-      <Stack.Screen name="Edit Dose" component={DosePerTime} initialParams={{fromManual: false}} options={{headerShown: false}} />
-
-      <Stack.Screen name="Next Dose" component={NextDose} options={{headerShown: false}} />
-      <Stack.Screen name="Time Dose" component={TimeDose} options={{headerShown: false}} />
-      <Stack.Screen name="Duration" component={SetDuration} options={{headerShown: false}} />
-
-      <Stack.Screen name="Edit Info" component={EditInfoScreen} options={{headerShown: false}}/>
-      <Stack.Screen name="Edit Reminder" component={EditReminderScreen} options={{headerShown: false}}/> */}
 
       <Stack.Screen name="Onboarding" component={Onboarding} options={{headerShown: false}}/>
 
